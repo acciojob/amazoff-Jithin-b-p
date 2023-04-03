8549d60d3ab8a8dea370e41236f7e9c9bf0246a0
unknown <jithinbp007@gmail.com>
2023-04-03 13:30:01 +0530

    first commit

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,13 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+
+        String[] dt = deliveryTime.split(":");
+        int hh = Integer.parseInt(dt[0]);
+        int mm = Integer.parseInt(dt[1]);
+
+        this.deliveryTime = hh * 60 + mm;
     }
 
     public String getId() {
---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,136 @@
+package com.driver;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class OrderRepository {
+
+    Map<String, Order> orderDb;
+    Map<String, DeliveryPartner> partnerDb;
+
+    Map<String, String> orderPartnerPairDb;
+
+    Map<String, List<String>> partnerOrderDb;
+
+
+
+    public void addOrder(Order order){
+
+        String orderId = order.getId();
+        orderDb.put(orderId, order);
+
+    }
+
+    public void addPartner(String partnerId){
+
+        DeliveryPartner deliveryPartner = new DeliveryPartner(partnerId);
+        partnerDb.put(partnerId, deliveryPartner);
+
+    }
+
+    public void addOrderPartnerPair(String orderId, String partnerId){
+
+        orderPartnerPairDb.put(orderId, partnerId);
+        if(!partnerOrderDb.containsKey(partnerId)){
+
+            partnerOrderDb.put(partnerId, new ArrayList<>());
+            partnerOrderDb.get(partnerId).add(orderId);
+        }else{
+
+            partnerOrderDb.get(partnerId).add(orderId);
+        }
+        partnerDb.get(partnerId).setNumberOfOrders(partnerDb.get(partnerId).getNumberOfOrders() + 1);
+
+    }
+
+    public Order getOrderById(String orderId){
+
+        return orderDb.get(orderId);
+
+    }
+
+    public DeliveryPartner getPartnerById(String partnerId){
+
+        return partnerDb.get(partnerId);
+
+    }
+
+    public int getOrderCountByPartnerId(String partnerId){
+
+        return partnerDb.get(partnerId).getNumberOfOrders();
+    }
+
+    public List<String> getOrdersByPartnerId(String partnerId){
+
+        return partnerOrderDb.get(partnerId);
+
+    }
+
+    public List<String> getAllOrders(){
+
+        List<String> orders = new ArrayList<>();
+
+        for(String orderId: orderDb.keySet()){
+
+            orders.add(orderId);
+        }
+
+        return orders;
+    }
+
+
+    public int getCountOfUnassignedOrders(){
+
+        int count = 0;
+
+        for(String orderId: orderDb.keySet()){
+
+            if(!orderPartnerPairDb.containsKey(orderId)){
+
+                count++;
+
+            }
+        }
+        return count;
+    }
+
+    public void deletePartnerById(String partnerId){
+
+        partnerDb.remove(partnerId);
+
+        for(String order: orderPartnerPairDb.keySet()){
+
+            if(orderPartnerPairDb.get(order).equals(partnerId)){
+
+                orderPartnerPairDb.remove(order);
+
+            }
+        }
+        partnerOrderDb.remove(partnerId);
+
+
+    }
+
+    public void deleteOrderById(String orderId){
+
+        orderDb.remove(orderId);
+
+        String partnerId = orderPartnerPairDb.get(orderId);
+        partnerDb.get(partnerId).setNumberOfOrders(partnerDb.get(partnerId).getNumberOfOrders() - 1);
+
+        orderPartnerPairDb.remove(orderId);
+
+        List<String> orders = partnerOrderDb.get(partnerId);
+
+        orders.remove(orderId);
+        partnerOrderDb.put(partnerId, orders);
+
+
+    }
+}
